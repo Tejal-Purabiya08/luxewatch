5a51e3234ed84bca9ec0e1b51227e756d0f17306
Teju <bhanu1983@gmail.com>
2026-05-17 21:15:52 +0530

    first commit

--- backend/Routes/productRoutes.js
+++ backend/Routes/productRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const Product = require("../model/productModel");
+const Product = require("../Model/productModel");
 
 
 // ➤ CREATE PRODUCT
